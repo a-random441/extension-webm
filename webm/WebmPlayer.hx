d42db6dd875b51ea7436ab951234aa330b138a29
KlavierGayming <82604411+KlavierGayming@users.noreply.github.com>
2021-07-21 07:53:07 +0200

    fixed funi error

--- webm/WebmPlayer.hx
+++ webm/WebmPlayer.hx
@@ -36,7 +36,7 @@ class WebmPlayer extends Bitmap
 	var lastDecodedVideoFrame = 0.0;
 	var lastRequestedVideoFrame = 0.0;
 	var playing = false;
-	var renderedCount = 0;
+	public var renderedCount = 0;
 	var renderedCount2 = 0;
 	var lastDecodedVideoFrame2 = 0.0;
 	var fkingElapsed = 0.0;
